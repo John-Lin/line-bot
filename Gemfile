4534e4355abf6e36410a83c30282692af8cbfa36
John-Lin <linton.tw@gmail.com>
2017-01-14 13:47:05 +0800

    first commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'thin'
+gem 'line-bot-api'
+
+group :development, :test do
+  gem 'config_env'
+end
+
+group :test do
+  gem 'rack'
+end
---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'sinatra'   # gem 'sinatra'
+require 'line/bot'  # gem 'line-bot-api'
+require 'config_env'
+
+ConfigEnv.path_to_config("#{__dir__}/config/config_env.rb")
+
+def client
+  @client ||= Line::Bot::Client.new { |config|
+    config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+  }
+end
+
+get '/ping' do
+  "PONG"
+end
+
+post '/callback' do
+  body = request.body.read
+
+  signature = request.env['HTTP_X_LINE_SIGNATURE']
+  unless client.validate_signature(body, signature)
+    error 400 do 'Bad Request' end
+  end
+
+  events = client.parse_events_from(body)
+
+  events.each { |event|
+    case event
+    when Line::Bot::Event::Message
+      case event.type
+      when Line::Bot::Event::MessageType::Text
+        message = {
+          type: 'text',
+          text: event.message['text']
+        }
+        client.reply_message(event['replyToken'], message)
+      end
+    end
+  }
+
+  "OK"
+end
